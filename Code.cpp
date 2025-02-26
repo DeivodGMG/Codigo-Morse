523e441d199b9ff32e3880888118ac53687a9071
DeivodGMG <153698627+DeivodGMG@users.noreply.github.com>
2025-02-26 14:04:18 -0600

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -9,12 +9,128 @@ cin >> EL_Mensaje;
 
 for (int i = 0 ; i < 500; i++)
 {
-    if (EL_Mensaje[i] == 'H' or EL_Mensaje[i] == 'a')
+    if (EL_Mensaje[i] == 'A' or EL_Mensaje[i] == 'a')
     {
-        cout << "Funciono";
-        break;
+        cout << ".-";
+      
     }
+    else if (EL_Mensaje[i] == 'B' or EL_Mensaje[i] == 'b')
+    {
+        cout << "-...";
+        
+    }
+     else if (EL_Mensaje[i] == 'C' or EL_Mensaje[i] == 'c')
+    {
+        cout << "-.-.";
+        
+    }
+     else if (EL_Mensaje[i] == 'D' or EL_Mensaje[i] == 'd')
+    {
+        cout << "-..";
+       
+    }
+     else if (EL_Mensaje[i] == 'E' or EL_Mensaje[i] == 'e')
+    {
+        cout << ".";
     
+    }
+     else if (EL_Mensaje[i] == 'F' or EL_Mensaje[i] == 'f')
+    {
+        cout << "..-.";
+       
+    }
+     else if (EL_Mensaje[i] == 'G' or EL_Mensaje[i] == 'g')
+    {
+        cout << "--.";
+      
+    }
+     else if (EL_Mensaje[i] == 'H' or EL_Mensaje[i] == 'h')
+    {
+        cout << "....";
+      
+    }
+     else if (EL_Mensaje[i] == 'I' or EL_Mensaje[i] == 'i')
+    {
+        cout << "..";
+       
+    }
+     else if (EL_Mensaje[i] == 'j' or EL_Mensaje[i] == 'j')
+    {
+        cout << ".---";
+       
+    }
+     else if (EL_Mensaje[i] == 'K' or EL_Mensaje[i] == 'k')
+    {
+        cout << "-.-";
+       
+    }
+     else if (EL_Mensaje[i] == 'L' or EL_Mensaje[i] == 'l')
+    {
+        cout << ".-..";
+       
+    }
+     else if (EL_Mensaje[i] == 'M' or EL_Mensaje[i] == 'm')
+    {
+        cout << "--";
+        
+    }
+     else if (EL_Mensaje[i] == 'N' or EL_Mensaje[i] == 'n')
+    {
+        cout << "-.";
+       
+    }
+     else if (EL_Mensaje[i] == 'O' or EL_Mensaje[i] == 'o')
+    {
+        cout << "---";
+        
+    }
+     else if (EL_Mensaje[i] == 'P' or EL_Mensaje[i] == 'p')
+    {
+        cout << ".--.";
+       
+    }
+     else if (EL_Mensaje[i] == 'Q' or EL_Mensaje[i] == 'q')
+    {
+        cout << "--.-";
+     
+    }
+     else if (EL_Mensaje[i] == 'R' or EL_Mensaje[i] == 'r')
+    {
+        cout << ".-.";
+       
+    }
+     else if (EL_Mensaje[i] == 'S' or EL_Mensaje[i] == 's')
+    {
+        cout << "...";
+    }
+     else if (EL_Mensaje[i] == 'T' or EL_Mensaje[i] == 't')
+    {
+        cout << "-";
+    }
+     else if (EL_Mensaje[i] == 'U' or EL_Mensaje[i] == 'u')
+    {
+        cout << "..-";
+    }
+     else if (EL_Mensaje[i] == 'V' or EL_Mensaje[i] == 'v')
+    {
+        cout << "...-";
+    }
+     else if (EL_Mensaje[i] == 'W' or EL_Mensaje[i] == 'w')
+    {
+        cout << ".--";
+    }
+     else if (EL_Mensaje[i] == 'X' or EL_Mensaje[i] == 'x')
+    {
+        cout << "-..-";
+    }
+     else if (EL_Mensaje[i] == 'Y' or EL_Mensaje[i] == 'y')
+    {
+        cout << "-.--";
+    }
+     else if (EL_Mensaje[i] == 'Z' or EL_Mensaje[i] == 'z')
+    {
+        cout << "--..";
+    }
 } //For
 }
 void Morse_a_Texto(){
