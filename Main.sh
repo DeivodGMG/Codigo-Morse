77018c03dc2a5b6d59d91db087bc8e3093fab32d
DeivodGMG <153698627+DeivodGMG@users.noreply.github.com>
2025-02-27 08:15:58 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 clear
-echo "** Traductor de Codigo Morse **"
-echo "1) De texto a morse"
-echo "2) De codigo morse a texto"
-echo ""
-echo "Ingrese un numero:"
+g++ Code.cpp -o Ejecutable
+echo "Pulse Enter para continuar"
+read skip
 ./Ejecutable
