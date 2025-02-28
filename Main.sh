689e539255f5e3af8395403e36a11555373f5f54
DeivodGMG <153698627+DeivodGMG@users.noreply.github.com>
2025-02-28 01:12:50 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
+
 clear
 g++ Code.cpp -o Ejecutable
-echo "Pulse Enter para continuar"
-read skip
-./Ejecutable
+echo "** El Menu Interactivo UPV **"
+    echo "1) De texto a Morse"
+    echo "2) De Morse a texto"
+    echo "Elija una opcion:"
+while true; do
+    read select
+    if [ "$select" -eq 1 ]; then
+        ./Ejecutable
+        break
+    elif [ "$select" -eq 2 ]; then
+        python3 oido.py
+        break
+    else
+        echo "Opción no válida, por favor intente de nuevo."
+    fi
+done
